691a6e7e8367bc86af90dd816f59e1c8e9d4055b
Andras Nett <andrasn@vantagecomputing.co.uk>
2019-01-25 10:30:13 +0000

    Delete Education, Experience, User, & Profile routes.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -10,7 +10,7 @@ const validateEducationInput = require('../../validation/education');
 
 // Load Profile Model
 const Profile = require('../../models/Profile');
-// Load User Profile
+// Load User Model
 const User = require('../../models/User');
 
 
@@ -221,6 +221,65 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
 
             profile.save().then(profile => res.json(profile));
         })
+});
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            // Get remove index
+            const removeIndex = profile.experience.map(item => item.id)
+                .indexOf(req.params.exp_id);
+
+            // Splice out of array
+            if (removeIndex === -1) {
+                return res.status(404).json({
+                    error: 'There is no experience with this ID',
+                });
+            }
+
+            profile.experience.splice(removeIndex, 1);
+
+            // Save
+            profile.save().then(profile => res.json(profile));
+        });
+});
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            // Get remove index
+            const removeIndex = profile.education.map(item => item.id)
+                .indexOf(req.params.edu_id);
+
+            // Splice out of array
+            if (removeIndex === -1) {
+                return res.status(404).json({
+                    error: 'There is no education with this ID',
+                });
+            }
+
+            profile.education.splice(removeIndex, 1);
+
+            // Save
+            profile.save().then(profile => res.json(profile));
+        });
+});
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+        User.findOneAndRemove({ _id: req.user.id }).then(() => 
+            res.json({ success: true })
+        );
+    });
 });
 
 module.exports = router;
