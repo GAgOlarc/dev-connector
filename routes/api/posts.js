f3fa58adeb116818f4f1c022f2550358e8d2694c
Andras Nett <andrasn@vantagecomputing.co.uk>
2019-01-25 16:48:39 +0000

    Add & Remove Comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -121,4 +121,56 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
         .catch(err => res.status(404).json(err));
 });
 
+// @route POST api/posts/comment/:id
+// @desc  Add comment to post
+// @access Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+        .then(post => {
+            const newComment = {
+                text: req.body.text,
+                name: req.user.name,
+                avatar: req.user.avatar,
+                user: req.user.id
+            }
+
+            //Add to comments array
+            post.comments.unshift(newComment);
+
+            post.save().then(post => res.status(200).json(post));
+        })
+        .catch(err => res.status(404).json('No comment found'));
+});
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc  Delete comment from post
+// @access Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            // Check to see if comment exists
+            if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+                return res.status(404).json('Comment does not exists');
+            }
+
+            // Get remove index
+            const removeIndex = post.comments.map(comment => comment._id.toString())
+                .indexOf(req.params.comment_id);
+
+            // Splice comment out of array
+            post.comments.splice(removeIndex, 1);
+
+            post.save().then(post => res.status(200).json(post));
+        })
+        .catch(err => res.status(404).json('No comment found'));
+});
+
 module.exports = router;
