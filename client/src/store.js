10509177783e746de9a1bbd411a68872b0c218b8
Your Name <you@example.com>
2019-02-03 14:55:52 +0000

    Registration & Error Reducer

--- client/src/store.js
+++ client/src/store.js
@@ -2,22 +2,22 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const initialState = {};
+// const initialState = {};
 
 const middleware = [thunk];
 
 const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
-    : compose;
- 
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        })
+        : compose;
+
 const enhancer = composeEnhancers(
-  applyMiddleware(...middleware)
-  // other store enhancers if any
+    applyMiddleware(...middleware)
+    // other store enhancers if any
 );
 
-const store = createStore(rootReducer, initialState, enhancer);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
