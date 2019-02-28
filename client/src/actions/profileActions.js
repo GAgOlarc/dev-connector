a25028b16e31119a6ca0af5921e0a54f51b220d7
Andras Nett <andras.nett@asolvi.com>
2019-02-28 17:11:48 +0000

    Add Dashboard Experience Display & Delete

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -50,6 +50,24 @@ export const addEducation = (eduData, history) => dispatch => {
         }));
 }
 
+// Delete Experience
+export const deleteExperience = (id) => dispatch => {
+    dispatch(setProfileLoading());
+    axios.delete(`/api/profile/experience/${id}`)
+        .then(res =>
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            })
+        )
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+}
+
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
     if (window.confirm('Are you sure? This can NOT be undone!')) {
@@ -58,8 +76,8 @@ export const deleteAccount = () => dispatch => {
                 dispatch({
                     type: SET_CURRENT_USER,
                     payload: {}
-                })
-            ).catch(err =>
+                }))
+            .catch(err =>
                 dispatch({
                     type: GET_ERRORS,
                     payload: err.response.data
