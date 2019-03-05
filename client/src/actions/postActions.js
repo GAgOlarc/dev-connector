23f9c6d889f5bb8efe55bf34a1c0da5536aa45f9
Andras Nett <andras.nett@asolvi.com>
2019-03-05 17:22:56 +0000

    Add PostItem Component & delete, like and unlike Posts

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -18,8 +18,8 @@ export const addPost = postData => dispatch => {
 }
 
 //Get Posts
-export const getPosts = () => dispatch => {
-    dispatch(setPostLoading());
+export const getPosts = (load = true) => dispatch => {
+    if (load) dispatch(setPostLoading());
     axios.get('/api/posts')
         .then(res => dispatch({
             type: GET_POSTS,
@@ -28,6 +28,41 @@ export const getPosts = () => dispatch => {
         .catch(err => dispatch({
             type: GET_POSTS,
             payload: null
+        }));
+}
+
+// Delete Post
+export const deletePost = id => dispatch => {
+    axios.delete('/api/posts/' + id)
+        .then(res => 
+            dispatch({
+                type: DELETE_POST,
+                payload: id
+            })
+        )
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }));
+}
+
+// Like Post
+export const addLike = id => dispatch => {
+    axios.post('/api/posts/like/' + id)
+        .then(res => dispatch(getPosts(false)))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }));
+}
+
+// Remove Like
+export const removeLike = id => dispatch => {
+    axios.post('/api/posts/unlike/' + id)
+        .then(res => dispatch(getPosts(false)))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
         }));
 }
 
